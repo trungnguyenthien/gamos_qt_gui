835450b90ea0fe4b0b75bf84e1f3ae6f1b7ec92f
Trung Nguyen <ngthientrung@gmail.com>
2023-11-22 19:09:33 -0500

    Format

--- src/model/BJsonValue.h
+++ src/model/BJsonValue.h
@@ -1,49 +1,36 @@
 #ifndef __BJSONVALUE_H__
 #define __BJSONVALUE_H__
 
+#include <QtCore/QList>
 #include <QtCore/QString>
 #include <QtCore/QStringList>
-#include <QtCore/QList>
 
-enum BJsonValueType
-{
-    MM,
-    CM,
-    M,
-    EV,
-    KEV,
-    XYZ_M
+enum BJsonValueType { MM, CM, M, EV, KEV, XYZ_M };
+
+struct BJsonValue {
+  BJsonValueType type;
+  QString value;
+  QString label;
+  bool isInputable;
+
+  QList<double> getDoubleValues() {
+    QStringList values = value.split(" ");
+    QList<double> output;
+    foreach (QString v, values) {
+      double doubleValue = v.toDouble();
+      output.append(doubleValue);
+    }
+    return output;
+  }
+
+  QList<QString> getStringValues() {
+    QStringList values = value.split(" ");
+    QList<QString> output;
+    foreach (QString v, values) {
+      output.append(v);
+    }
+    return output;
+  }
 };
 
-struct BJsonValue
-{
-    BJsonValueType type;
-    QString value;
-    QString label;
-    bool isInputable;
-
-    QList<double> getDoubleValues()
-    {
-        QStringList values = value.split(" ");
-        QList<double> output;
-        foreach (QString v, values)
-        {
-            double doubleValue = v.toDouble();
-            output.append(doubleValue);
-        }
-        return output;
-    }
-
-    QList<QString> getStringValues()
-    {
-        QStringList values = value.split(" ");
-        QList<QString> output;
-        foreach (QString v, values)
-        {
-            output.append(v);
-        }
-        return output;
-    }
-};
-
-#endif // __BJSONVALUE_H__
+#endif  // __BJSONVALUE_H__
