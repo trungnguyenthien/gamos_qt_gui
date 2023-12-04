87717158adfda9e27f88c144fdf83a0aa4cc569a
Trung Nguyen <ngthientrung@gmail.com>
2023-12-04 10:55:26 -0500

    Just Commit

--- src/widget/Terminal.cpp
+++ src/widget/Terminal.cpp
@@ -1,2 +1,16 @@
 #include "Terminal.h"
 void Terminal::setCurrentDir(QString dir) { proc.setWorkingDirectory(dir); }
+
+Terminal::Terminal(QWidget *parent) : QWidget(parent) {
+  grid = new QGridLayout(this);
+  terminalOutput = new QTextEdit(this);
+  setLayout(grid);
+
+  // connect(startButton, &QPushButton::clicked, this, &MyWidget::startProcess);
+  connect(&process, &QProcess::readyReadStandardOutput, this,
+          &Terminal::readOutput);
+  connect(&process, &QProcess::finished, this, &Terminal::processFinished);
+}
+
+private slots:
+void Terminal::processFinished(int exitCode, QProcess::ExitStatus exitStatus) {}
--- src/widget/Terminal.h
+++ src/widget/Terminal.h
@@ -1,13 +1,25 @@
 #ifndef __TERMINAL_H__
 #define __TERMINAL_H__
 
+#include <QGridLayout>
 #include <QProcess>
+#include <QTextEdit>
+#include <QtCore/QString>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QWidget>
 #include <iostream>
-
 using namespace std;
-class Terminal {
+class Terminal : public QWidget {
+  Q_OBJECT
+ private:
+  QGridLayout *grid;
+  slots void processFinished(int exitCode, QProcess::ExitStatus exitStatus);
+
  public:
-  QProcess proc;
+  QProcess process;
+  QTextEdit *terminalOutput;
+  Terminal(QWidget *parent);
   void setCurrentDir(QString dir);
 };
 #endif  // __TERMINAL_H__
